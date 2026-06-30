0ddfa234a04084c6478779a7eecaada3d1bb7b13
LEE HANA <127467192+LeEHaNa1@users.noreply.github.com>
2026-06-30 23:16:59 +0900

    [Passed] 최대공약수 구하기 (2ms, 0MB)

---
+++ trail2/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void getCGD(int n, int m){
+    int rest = 0;
+    rest = n % m;
+    while(rest != 0){
+        n = m;
+        m = rest;
+        rest = n % m;
+    }
+    cout << m;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    getCGD(n,m);
+    return 0;
+}
